579f6cc9e8483ef97066746b246662e19c863c89
emoji <emoji@emojideMBP.lan>
2023-11-22 13:44:55 +0800

    继续完善image request

--- src/api/create_image.rs
+++ src/api/create_image.rs
@@ -8,34 +8,34 @@ use serde::{Deserialize, Serialize};
 pub struct CreateImageRequest {
     /// A text description of the desired image(s). The maximum length is 4000 characters for dall-e-3.
     #[builder(setter(into))]
-    pub prompt: String,
+    prompt: String,
     /// The model to use for image generation. Only support Dall-e-3
     #[builder(default)]
-    pub model: ImageModel,
+    model: ImageModel,
     /// The number of images to generate. Must be between 1 and 10. For dall-e-3, only n=1 is supported.
     #[builder(default, setter(strip_option))]
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub n: Option<usize>,
+    n: Option<usize>,
     /// The quality of the image that will be generated. hd creates images with finer details and greater consistency across the image. This param is only supported for dall-e-3.
     #[builder(default, setter(strip_option))]
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub quality: Option<ImageQuality>,
+    quality: Option<ImageQuality>,
     /// The format in which the generated images are returned. Must be one of url or b64_json
     #[builder(default, setter(strip_option))]
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub response_format: Option<ImageResponseFormat>,
+    response_format: Option<ImageResponseFormat>,
     /// The size of the generated images. Must be one of 1024x1024, 1792x1024, or 1024x1792 for dall-e-3 models.
     #[builder(default, setter(strip_option))]
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub size: Option<ImageSize>,
+    size: Option<ImageSize>,
     /// The style of the generated images. Must be one of vivid or natural. Vivid causes the model to lean towards generating hyper-real and dramatic images. Natural causes the model to produce more natural, less hyper-real looking images. This param is only supported for dall-e-3.
     #[builder(default, setter(strip_option))]
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub style: Option<ImageStyle>,
+    style: Option<ImageStyle>,
     /// A unique identifier representing your end-user, which can help OpenAI to monitor and detect abuse.
     #[builder(default, setter(strip_option, into))]
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub user: Option<String>,
+    user: Option<String>,
 }
 
 #[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Serialize)]
