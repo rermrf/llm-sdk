2b901af004b319b8a61d20bf3470e20add66f0ec
emoji <emoji@emojideMacBook-Pro.local>
2023-11-24 09:38:53 +0800

    完善

--- src/api/chat_completion.rs
+++ src/api/chat_completion.rs
@@ -16,11 +16,22 @@ pub struct ChatCompletionRequest {
 #[derive(Debug, Clone, Serialize)]
 pub enum ChatCompletionMessage {
     /// A message from a system
-    System(String),
+    System(SystemMessage),
     /// A message from a human
     User(String),
-    /// A message from the model
-    Model(String),
+    /// A message from the Assistant
+    Assistant(String),
+    /// A message from a Tool
+    Tool(String),
+}
+
+#[derive(Debug, Clone, Serialize)]
+pub struct SystemMessage{
+    /// The contents of the system message.
+    content: String,
+    /// An optional name for the participant. Provides the model information to differentiate between participants of the same role.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    name: Option<String>,
 }
 
 #[derive(Debug, Clone, Deserialize)]
